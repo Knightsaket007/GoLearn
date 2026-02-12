5f0586b79ba6a4095608e46437b93ce6a970c88d
Knightsaket007 <saketsourav3@gmail.com>
2026-02-12 23:59:37 +0530

    run the struct

--- 11_mystructs/main.go
+++ 11_mystructs/main.go
@@ -4,4 +4,16 @@ import "fmt"
 
 func main(){
 	fmt.Println("structs in golang")
+
+	me:=User{"Saket", "saket@mail.com", true, 25}
+
+	fmt.Println(me);
+	fmt.Println("Myy details are: %+v", me)
+}
+
+type User struct{
+	Name string 
+	Email string
+	Status bool
+	Age int
 }
