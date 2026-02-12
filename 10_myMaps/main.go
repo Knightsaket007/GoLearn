8c6ec4fabef3e550b3663933773d28de2eb131b0
Knightsaket007 <saketsourav3@gmail.com>
2026-02-12 23:23:52 +0530

    loop and deletion of map

--- 10_myMaps/main.go
+++ 10_myMaps/main.go
@@ -10,7 +10,18 @@ func main() {
 
 	language["go"]="golang";
 	language["js"]="Javascript";
+	language["rb"]="Ruby";
 	language["ts"]="Typescript";
 
 	fmt.Println("List of langusge...", language)
+	fmt.Println("Js short of...", language["js"])
+	
+	// =-=--= Delete -=-=-=//
+	delete(language,"rb")
+	fmt.Println("List of langusge...", language)
+
+	// --=--= loop =-=-=-//
+	for key ,value := range language{
+		fmt.Printf("For key %v, the value is %v\n", key, value)
+	}
 }
