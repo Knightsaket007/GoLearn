2e53d008c1cb82f5481f1cf7d7e6edbf6690cebd
Knightsaket007 <saketsourav3@gmail.com>
2026-02-27 23:57:47 +0530

    function done

--- 15_function/main.go
+++ 15_function/main.go
@@ -7,23 +7,23 @@ func main() {
 
 	// sum:= adder(34, 32);
 	// fmt.Printf("sum of 2 number is... %v", sum)
-	sum:=proAdder(1,2,3,4)
-	fmt.Printf("sum of 2 number is... %v", sum)
+	sum, length:=proAdder(1,2,3,4)
+	fmt.Printf("sum of multiple numbers is: %v and length of input: %v", sum, length)
 }
 
 func adder(valOne int, valTwo int) int{
 	return valOne + valTwo;
 }
 
-func proAdder(values ...int) int {
+func proAdder(values ...int) (int, int) {
 	sum:=0
 	if len(values)<=0{
-		return sum;
+		return sum, len(values);
 	}
 
 	for _, val := range values{
 		// fmt.Println("val...", val)
 		sum+=val;
 	}
-	return sum;
+	return sum, len(values);
 }
