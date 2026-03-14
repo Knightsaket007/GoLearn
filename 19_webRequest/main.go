7d1d363a1bceee36a7aed12a660b4827b407cbe1
Knightsaket007 <saketsourav3@gmail.com>
2026-03-14 23:41:59 +0530

    .

--- 19_webRequest/main.go
+++ 19_webRequest/main.go
@@ -2,19 +2,24 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
-const url ="https://google.com"
+const url = "https://google.com"
 
 func main() {
 	fmt.Println("Web request")
 
-	response, err := http.Get(url);
+	response, err := http.Get(url)
 
-	if err!= nil{
-		panic(err);
+	if err != nil {
+		panic(err)
 	}
 
 	fmt.Printf("Response is of type: %T\n", response)
+
+	response.Body.Close() //=-=--=-close the connection =-=-=-=-//
+
+	databyte, err := ioutil.ReadAll(response.Body)
 }
