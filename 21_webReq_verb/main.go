8939989aaf1926cbc3d2abe8216216e3ef2bd8ad
Knightsaket007 <saketsourav3@gmail.com>
2026-03-16 23:56:30 +0530

    more on web req

--- 21_webReq_verb/main.go
+++ 21_webReq_verb/main.go
@@ -1,11 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+)
 
 func main() {
 	fmt.Println("Welcome to web verb")
 }
 
 func PerformGetReq(){
-	
+
+	const myurl="https://localhost:8000/get"
+
+	response, err:=http.Get(myurl)
+
+	if err!=nil{
+		panic(err);
+	}
+
+	defer response.Body.Close();
+
+	fmt.Println("Status Code : ", response.StatusCode);
+	fmt.Println("Content length is:", response.ContentLength)
+
+	var responseString strings.Builder
+	content,_:= io.ReadAll(response.Body)
 }
