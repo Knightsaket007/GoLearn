d9f81390374027436f00705f4cb48f231efa8db1
Knightsaket007 <saketsourav3@gmail.com>
2026-03-26 13:44:20 +0530

    post form request

--- 21_webReq_verb/main.go
+++ 21_webReq_verb/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 	"strings"
 )
 
@@ -11,6 +12,7 @@ func main() {
 	fmt.Println("Welcome to web verb")
 	PerformGetReq()
 	PerformPostJsonRequest()
+	PerformPostFormRequest()
 }
 
 func PerformGetReq(){
@@ -66,4 +68,25 @@ func PerformPostJsonRequest(){
 	fmt.Println(string(content))
 
 
+}
+
+func PerformPostFormRequest(){
+	const myurl="http://localhost:8000/postform";
+
+	data:= url.Values{};
+	data.Add("firstName", "Saket")
+	data.Add("lastName", "Sourav")
+	data.Add("email", "saket@go.dev")
+
+	response, err:=http.PostForm(myurl, data);
+
+	if err !=nil{
+		panic(err)
+	}
+
+	defer response.Body.Close()
+
+	content, _:=io.ReadAll(response.Body)
+
+	fmt.Println("response from Post Form req...", string(content))
 }
