f64fb1c3665113a47082fdb9f45d303782802cdc
Knightsaket007 <saketsourav3@gmail.com>
2026-03-29 22:23:00 +0530

    unmarshal or decode json

--- 22_bitmorejson/main.go
+++ 22_bitmorejson/main.go
@@ -5,34 +5,59 @@ import (
 	"fmt"
 )
 
-type team struct{
-	Name string `json:"TeamName"`
-	Members int16 `json:"bande"`
-	Rank int `json:"position"`
-	Password string 
+type team struct {
+	Name           string `json:"TeamName"`
+	Members        int16  `json:"bande"`
+	Rank           int    `json:"position"`
+	Password       string
 	PopularMembers []string `json:"heros,omitempty"`
 }
 
 func main() {
 	fmt.Println("Welcome to JSON")
-	EncodeJSon()
+	// EncodeJSon()
+	DecodeJson()
 }
 
-func EncodeJSon(){
-	MCU_team:= []team{
+func EncodeJSon() {
+	MCU_team := []team{
 		{"Avengers", 10, 1, "hydra ki jai", []string{"Iron Man", "spider man", "Captain America"}},
 		{"x men", 12, 2, "hello123", []string{"wolverine", "magnito", "X javier"}},
 		{"thunderbolt", 5, 3, "marvel123", []string{"Sentry", "winter soldier"}},
 		{"teamX", 6, 4, "marvel1234", []string{}},
 	}
 
-	finalJson, err:= json.MarshalIndent(MCU_team, "", "\t")
+	finalJson, err := json.MarshalIndent(MCU_team, "", "\t")
 
-	if err !=nil{
+	if err != nil {
 		panic(err)
 	}
 
 	fmt.Printf("%s\n", finalJson)
 
+}
 
+func DecodeJson() {
+	jsonDatafromWeb := []byte(`
+	   {
+           "TeamName": "Avengers",
+           "bande": 10,
+           "position": 1,
+           "Password": "",
+           "heros": ["Iron Man", "spider man", "Captain America"]
+       }
+	`)
+
+	var teams team;
+
+	checkValid := json.Valid(jsonDatafromWeb)
+
+	if checkValid{
+		fmt.Println("Valid Json");
+		json.Unmarshal(jsonDatafromWeb, &teams)
+		
+		fmt.Printf("%#v\n", teams)
+	}else{
+		fmt.Println("Json is not valid")
+	}
 }
