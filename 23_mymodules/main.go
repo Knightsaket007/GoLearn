c8cfc6d88f93f99a74c85554d0a12e3df0d4a673
Knightsaket007 <saketsourav3@gmail.com>
2026-04-02 22:59:38 +0530

    go server from gorilla/mux

--- 23_mymodules/main.go
+++ 23_mymodules/main.go
@@ -1,10 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-
 	fmt.Println("Hello mod in Golang")
+
+	r:=mux.NewRouter();
+	r.HandleFunc("/", serveHome).Methods("GET")
+
+    log.Fatal(http.ListenAndServe(":4000", r))
 }
 
 //=--=-==- API -=--==-//
+
+func serveHome(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("<h1>Welcome to Golang...</h1>"))
+}
