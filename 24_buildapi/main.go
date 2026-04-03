9234459c1086d3eeb03e808949d4b5a381124fda
Knightsaket007 <saketsourav3@gmail.com>
2026-04-03 23:49:26 +0530

    model

--- 24_buildapi/main.go
+++ 24_buildapi/main.go
@@ -3,11 +3,13 @@ package main
 type Team struct{
  TeamId string `json:"tid"`
  TeamName string `json:"tname"`
- TeamPrice string 
+ TeamPrice int `json:"price"`
+ Hero *Hero `json:"hero"`
 }
 
 type Hero struct{
-
+ FullName string `json:"fullname"`
+ Website string `json:"website"`
 }
 
 func main(){
