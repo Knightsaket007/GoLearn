a5113f4ccfff23fe864b12c56e26a535dd88faf3
Knightsaket007 <saketsourav3@gmail.com>
2026-04-03 17:16:22 +0530

    api

---
+++ 24_buildapi/go.mod
@@ -0,0 +1,5 @@
+module github.com/Knightsaket007/buildapi
+
+go 1.25.1
+
+require github.com/gorilla/mux v1.8.1 // indirect
---
+++ 24_buildapi/main.go
@@ -0,0 +1,15 @@
+package main
+
+type Team struct{
+ TeamId string `json:"tid"`
+ TeamName string `json:"tname"`
+ TeamPrice string 
+}
+
+type Hero struct{
+
+}
+
+func main(){
+
+}
