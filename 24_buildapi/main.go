28dd2f94e6bad42543c3fa129e88120b0b04879e
Knightsaket007 <saketsourav3@gmail.com>
2026-04-04 23:57:43 +0530

    controller

--- 24_buildapi/main.go
+++ 24_buildapi/main.go
@@ -1,28 +1,33 @@
 package main
 
-type Team struct{
- TeamId string `json:"tid"`
- TeamName string `json:"tname"`
- TeamPrice int `json:"price"`
- Hero *Hero `json:"hero"`
+import "fmt"
+
+type Team struct {
+	TeamId    string `json:"tid"`
+	TeamName  string `json:"tname"`
+	TeamPrice int    `json:"price"`
+	Hero      *Hero  `json:"hero"`
 }
 
-type Hero struct{
- FullName string `json:"fullname"`
- Website string `json:"website"`
+type Hero struct {
+	FullName string `json:"fullname"`
+	Website  string `json:"website"`
 }
 
 // =--=- FAke DB ==--=-//
 
-var team[] Team;
+var team []Team
 
-
-//=--=-= Middleware, helper -=-==-//
-func (t *Team) IsEmpty() bool{
-	return t.TeamId =="" && t.TeamName=="";
+// =--=-= Middleware, helper -=-==-//
+func (t *Team) IsEmpty() bool {
+	return t.TeamId == "" && t.TeamName == ""
 }
 
+func main() {
 
-func main(){
+}
 
+// =-=- serve home route -=-=-///
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	w.write([]byte("<h1>Welcome to the Team API</h1>"))
 }
